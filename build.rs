35e70ad178966ac14e410f56a06bc21eca926228
Mason Stallmo <mason@kaliberlabs.com>
2025-06-11 13:50:21 -0700

    chore: Update dependencies
    - Update `tonic` to 0.13
    - Update `prost` to 0.13
    - Update `tonic-build` to 0.13
    - Update `Cargo.toml` fields

--- build.rs
+++ build.rs
@@ -1,10 +1,11 @@
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(false)
         .out_dir("src") // you can change the generated code's location
-        .compile(
+        .compile_protos(
             &["common/protobuf/grpc_service.proto"],
             &["common/protobuf"], // specify the root location to search proto dependencies
-        )
-        .unwrap();
+        )?;
+
+    Ok(())
 }
